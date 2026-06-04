ff04d4d7225aa7976a9c29863921cc3ed2310558
Seito <j.s.coleman00@gmail.com>
2026-06-04 19:01:16 -0400

    Bump Android version to 1.0.5 (versionCode 6)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "app.roam.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 6
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
