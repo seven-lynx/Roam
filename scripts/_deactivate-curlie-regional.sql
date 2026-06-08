3f96a9603086b5684432d697725ec9f2be521938
Seito <j.s.coleman00@gmail.com>
2026-06-07 22:35:18 -0400

    chore: gitignore scripts/ directory (proprietary sources & seed pool)

--- scripts/_deactivate-curlie-regional.sql
+++
@@ -1,9 +0,0 @@
--- Disable statement timeout for this bulk cleanup operation, then deactivate
--- all Curlie URLs in the People & Places category.
-SET statement_timeout = 0;
-
-UPDATE urls
-SET inactive = true
-WHERE source = 'curlie'
-  AND category_id = 'c1000000-0000-0000-0000-000000000007'
-  AND inactive = false;
